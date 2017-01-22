ba35adf39f9ce4be4f31ff3e856c2a8ab1046c3d
gamg <adolfz10@gmail.com>
2017-01-22 15:51:20 -0400

    Comenzando proyecto

---
+++ app/Models/Task.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Taskapp\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Task extends Model
+{
+    protected $table = "tasks";
+
+    protected $fillable = ['user_id', 'name'];
+
+    public function user()
+    {
+        return $this->belongsTo('Taskapp\Models\User');
+    }
+}
---
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->string('password');
+            $table->integer('type')->default(2); // 1 or 2
+            $table->boolean('active')->default(true);
+            $table->rememberToken();
+            $table->timestamps(); // created_at, updated_at
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('users');
+    }
+}
