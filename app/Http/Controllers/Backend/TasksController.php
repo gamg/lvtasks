c079e43ed2866d11a10337160a32e4882123a0a6
gamg <adolfz10@gmail.com>
2017-02-10 19:32:25 -0400

    se crearon controladores para usuarios y tareas

---
+++ app/Http/Controllers/Backend/TasksController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Taskapp\Http\Controllers\Backend;
+
+use Illuminate\Http\Request;
+use Taskapp\Http\Controllers\Controller;
+
+class TasksController extends Controller
+{
+    //
+}
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,8 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::group(['namespace' => 'Backend'], function (){
+    Route::resource('users', 'UsersController');
+});
+
